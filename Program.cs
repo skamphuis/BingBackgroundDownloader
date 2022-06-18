88038ea4f5ea99243ff2a42e20fe4f0b57e039fa
Stefan Kamphuis <stefan@superska.net>
2022-06-18 16:27:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Web;
+using HtmlAgilityPack;
+
+var url = "https://www.bing.com";
+var web = new HtmlWeb();
+var doc = web.Load(url);
+
+var imgDiv = doc.DocumentNode.SelectSingleNode("//div[@class='img_cont']");
+if (imgDiv != null)
+{
+// <div class="img_cont" style="background-image: url(/th?id=OHR.CelebratingSurfing_ROW9691697820_1920x1080.jpg&amp;rf=LaDigue_1920x1080.jpg); opacity: 1;"><div class="shaders"><div class="topLeft"></div><div class="topRight"></div><div class="bottom"></div></div></div>
+
+    var style = imgDiv.Attributes["style"].Value;
+
+    var rx = new Regex("url\\((.*)\\)");
+    if (rx.IsMatch(style))
+    {
+        var imgUrl = HttpUtility.HtmlDecode("https://www.bing.com" + rx.Match(style).Groups[1].Value);
+        var dest = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "BingImages");
+        if (!Directory.Exists(dest))
+        {
+            Directory.CreateDirectory(dest);
+        }
+
+        using var client = new WebClient();
+        client.DownloadFile(new Uri(imgUrl), Path.Combine(dest, $"{DateTime.Now:yyyy-MM-dd}.jpg"));
+    }
+}
