25852515db6ee129f3d7dc43409255b6400fd5a9
Stefan Kamphuis <stefan@superska.net>
2022-06-20 09:06:02 +0200

    add log4net

---
+++ Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace BingBackgroundDownloader
+{
+    internal class Logger
+    {
+        public static ILog GetLogger(string name) => LogManager.GetLogger(name);
+
+        public static ILog GetLogger(Type type) => LogManager.GetLogger(type);
+
+        private static ILog _logger = null;
+        internal static ILog Instance
+        {
+            get
+            {
+                if (_logger == null)
+                {
+                    _logger = LogManager.GetLogger("BingBackgroundDownloader");
+
+                    XmlConfigurator.Configure(new FileInfo(Path.Combine(AppContext.BaseDirectory, "Log4Net.config")));
+                }
+
+                return _logger;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,30 +4,48 @@ using System.Net;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Web;
+using BingBackgroundDownloader;
 using HtmlAgilityPack;
 
-var url = "https://www.bing.com";
-var web = new HtmlWeb();
-var doc = web.Load(url);
-
-var imgDiv = doc.DocumentNode.SelectSingleNode("//div[@class='img_cont']");
-if (imgDiv != null)
+try
 {
-// <div class="img_cont" style="background-image: url(/th?id=OHR.CelebratingSurfing_ROW9691697820_1920x1080.jpg&amp;rf=LaDigue_1920x1080.jpg); opacity: 1;"><div class="shaders"><div class="topLeft"></div><div class="topRight"></div><div class="bottom"></div></div></div>
+    var url = "https://www.bing.com";
+    var web = new HtmlWeb();
+    var doc = web.Load(url);
 
-    var style = imgDiv.Attributes["style"].Value;
-
-    var rx = new Regex("url\\((.*)\\)");
-    if (rx.IsMatch(style))
+    var imgDiv = doc.DocumentNode.SelectSingleNode("//div[@class='img_cont']");
+    if (imgDiv != null)
     {
-        var imgUrl = HttpUtility.HtmlDecode("https://www.bing.com" + rx.Match(style).Groups[1].Value);
-        var dest = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "BingImages");
-        if (!Directory.Exists(dest))
-        {
-            Directory.CreateDirectory(dest);
-        }
+// <div class="img_cont" style="background-image: url(/th?id=OHR.CelebratingSurfing_ROW9691697820_1920x1080.jpg&amp;rf=LaDigue_1920x1080.jpg); opacity: 1;"><div class="shaders"><div class="topLeft"></div><div class="topRight"></div><div class="bottom"></div></div></div>
+        Logger.Instance.Debug("Image div found");
 
-        using var client = new WebClient();
-        client.DownloadFile(new Uri(imgUrl), Path.Combine(dest, $"{DateTime.Now:yyyy-MM-dd}.jpg"));
+        var style = imgDiv.Attributes["style"].Value;
+
+        var rx = new Regex("url\\((.*)\\)");
+        if (rx.IsMatch(style))
+        {
+            Logger.Instance.Debug("No URL found in style attribute");
+            var imgUrl = HttpUtility.HtmlDecode("https://www.bing.com" + rx.Match(style).Groups[1].Value);
+            var dest = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "BingImages");
+            if (!Directory.Exists(dest))
+            {
+                Directory.CreateDirectory(dest);
+            }
+
+            Logger.Instance.Debug($"Downloading image from: {imgUrl}");
+            using var client = new WebClient();
+            client.DownloadFile(new Uri(imgUrl), Path.Combine(dest, $"{DateTime.Now:yyyy-MM-dd}.jpg"));
+            Logger.Instance.Debug($"Saved image to: {Path.Combine(dest, $"{DateTime.Now:yyyy-MM-dd}.jpg")}");
+        }
     }
+    else
+    {
+        Logger.Instance.Debug("No image div found");
+    }
+
+    Logger.Instance.Debug($"Done");
+}
+catch (Exception e)
+{
+    Logger.Instance.Error(e);
 }
